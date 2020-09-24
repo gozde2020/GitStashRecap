283d8c464a64b524a706cc0171c098e45fcf4568
gozde2020 <ercevik.gozde@gmail.com>
2020-09-24 16:57:33 -0700

    Testrecap file modified

--- src/com/stash/TestRecap.java
+++ src/com/stash/TestRecap.java
@@ -1,5 +1,8 @@
 package com.stash;
 
 public class TestRecap {
-
+	public static void main(String[] args) {
+		System.out.println("Modification of our code");
+		System.out.println("hello world");
+	}
 }
