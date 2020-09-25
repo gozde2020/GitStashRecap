309bb109b3de75dfbf55bf280f458f18f6eb389f
gozde2020 <ercevik.gozde@gmail.com>
2020-09-24 17:48:32 -0700

    added featureA

--- src/com/stash/TestRecap.java
+++ src/com/stash/TestRecap.java
@@ -4,5 +4,9 @@ public class TestRecap {
 	public static void main(String[] args) {
 		System.out.println("Modification of our code");
 		System.out.println("hello world");
+		
+		System.out.println("git stash test");
+		
+		System.out.println("Hello sf");
 	}
 }
